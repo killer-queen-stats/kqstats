0a74674be753cc1793297f58bfa2366c3cf1a81c
Kevin Snyder <snyderk1@seattleu.edu>
2018-06-21 20:11:01 -0700

    Refactor KQStream unit tests
    Removed `createListenerPromise()`. Now each message received by
    `KQStream` is individually awaited, instead of awaiting a certain number
    of messages to be received, which could be misleading.
    
    For `KQStream#read` unit tests, `sleep-promise` is used. This is not
    ideal and should be changed to use a different approach in the future.

--- test/KQStream.ts
+++ test/KQStream.ts
@@ -3,7 +3,7 @@ import * as http from 'http';
 import * as sinon from 'sinon';
 import * as websocket from 'websocket';
 import { KQStream, Events, PlayerNames, PlayerKill, Character } from '../src/lib/KQStream';
-import { sleep } from '../src/lib/helper';
+import * as sleep from 'sleep-promise';
 import { KQCab } from '../src/lib/KQCab';
 
 interface ListenerCount {
@@ -11,22 +11,19 @@ interface ListenerCount {
     onPlayerKill?: number;
 }
 
-function createListenerPromise<K extends keyof Events>(
-    stream: KQStream,
-    event: K,
-    listener: (event: Events[K]) => void,
-    numCalls: number
-) {
-    let count = 0;
-    return new Promise<void>((resolve) => {
-        stream.on(event, (arg) => {
-            listener(arg);
-            if (++count === numCalls) {
-                resolve();
-            }
-        });
-    });
-}
+type TestEvent = {
+    timestamp: string;
+    type: keyof Events;
+    data: string;
+};
+
+type CabEvents = {
+    [K in keyof Events]: Events[K][];
+};
+
+type CabPromises = {
+    [K in keyof Events]: Promise<void>[];
+};
 
 /**
  * Removes all event listeners from a KQStream.
@@ -42,142 +39,134 @@ function removeAllListeners(stream: KQStream) {
 }
 
 describe('KQStream', () => {
-    interface TestEvent {
-        timestamp: string;
-        message: string;
-    }
-
     const stream = new KQStream();
-    const events: TestEvent[] = [{
+    const messages: TestEvent[] = [{
         timestamp: '1518063130441',
-        message: '![k[playernames],v[,,,,,,,,,]]!'
+        type: 'playernames',
+        data: '![k[playernames],v[,,,,,,,,,]]!'
     }, {
         timestamp: '1518063130451',
-        message: '![k[playerKill],v[730,860,9,8]]!'
+        type: 'playerKill',
+        data: '![k[playerKill],v[730,860,9,8]]!'
     }, {
         timestamp: '1518063130461',
-        message: '![k[playerKill],v[770,860,9,10]]!'
+        type: 'playerKill',
+        data: '![k[playerKill],v[770,860,9,10]]!'
     }, {
         timestamp: '1518063130471',
-        message: '![k[playerKill],v[810,860,9,4]]!'
+        type: 'playerKill',
+        data: '![k[playerKill],v[810,860,9,4]]!'
     }, {
         timestamp: '1518063130481',
-        message: '![k[playerKill],v[1071.977,20,3,10]]!'
+        type: 'playerKill',
+        data: '![k[playerKill],v[1071.977,20,3,10]]!'
     }];
-    const expectedPlayerNamesEvents: PlayerNames[] = [{}];
-    const expectedPlayerKillEvents: PlayerKill[] = [{
-        pos: {
-            x: 770,
-            y: 860
-        },
-        killed: Character.BlueChecks,
-        by: Character.GoldChecks
-    }, {
-        pos: {
-            x: 770,
-            y: 860
-        },
-        killed: Character.BlueChecks,
-        by: Character.GoldChecks
-    }, {
-        pos: {
-            x: 810,
-            y: 860
-        },
-        killed: Character.BlueStripes,
-        by: Character.GoldChecks
-    }, {
-        pos: {
-            x: 1071.977,
-            y: 20
-        },
-        killed: Character.BlueChecks,
-        by: Character.GoldStripes
-    }];
-    const onPlayerNames = (event: PlayerNames) => {
-        playerNamesEvents.push(event);
-    };
-    const onPlayerKill = (event: PlayerKill) => {
-        playerKillEvents.push(event);
+    const expectedEvents: CabEvents = {
+        playernames: [{}],
+        playerKill: [{
+            pos: {
+                x: 770,
+                y: 860
+            },
+            killed: Character.BlueChecks,
+            by: Character.GoldChecks
+        }, {
+            pos: {
+                x: 770,
+                y: 860
+            },
+            killed: Character.BlueChecks,
+            by: Character.GoldChecks
+        }, {
+            pos: {
+                x: 810,
+                y: 860
+            },
+            killed: Character.BlueStripes,
+            by: Character.GoldChecks
+        }, {
+            pos: {
+                x: 1071.977,
+                y: 20
+            },
+            killed: Character.BlueChecks,
+            by: Character.GoldStripes
+        }]
     };
 
-    let playerNamesEvents: PlayerNames[];
-    let playerKillEvents: PlayerKill[];
-    let playerNamesPromise: Promise<void>;
-    let playerKillPromise: Promise<void>;
+    let receivedEvents: CabEvents;
 
     describe('#connect', () => {
         before(async () => {
+            receivedEvents = {
+                playernames: [],
+                playerKill: []
+            };
+
             const cab = new KQCab();
             await stream.connect(`ws://localhost:${KQCab.DEFAULT_PORT}`);
 
-            // Arrange
-            playerNamesEvents = [];
-            playerKillEvents = [];
-            playerNamesPromise = createListenerPromise(stream, 'playernames', onPlayerNames, 1);
-            playerKillPromise = createListenerPromise(stream, 'playerKill', onPlayerKill, 4);
-
-            // Act
-            for (let event of events) {
-                cab.send(event.message);
+            for (let message of messages) {
+                await new Promise<void>((resolve) => {
+                    stream.once(message.type, (arg) => {
+                        (receivedEvents[message.type] as any).push(arg);
+                        resolve();
+                    });
+                    cab.send(message.data);
+                });
             }
         });
 
         it('should process playernames events', async () => {
-            await playerNamesPromise;
-            expect(playerNamesEvents.length).to.equal(1);
-            for (let expectedEvent of expectedPlayerNamesEvents) {
-                expect(playerNamesEvents).to.deep.include(expectedEvent);
+            expect(receivedEvents.playernames.length).to.equal(1);
+            for (let expectedEvent of expectedEvents.playernames) {
+                expect(receivedEvents.playernames).to.deep.include(expectedEvent);
             }
         });
 
         it('should process playerKill events', async () => {
-            await playerKillPromise;
-            expect(playerKillEvents.length).to.equal(4);
-            for (let expectedEvent of expectedPlayerKillEvents) {
-                expect(playerKillEvents).to.deep.include(expectedEvent);
+            expect(receivedEvents.playerKill.length).to.equal(4);
+            for (let expectedEvent of expectedEvents.playerKill) {
+                expect(receivedEvents.playerKill).to.deep.include(expectedEvent);
             }
-        });
-
-        after(() => {
-            removeAllListeners(stream);
         });
     });
 
     describe('#read', () => {
-        before(() => {
-            // Arrange
-            playerNamesEvents = [];
-            playerKillEvents = [];
-            playerNamesPromise = createListenerPromise(stream, 'playernames', onPlayerNames, 1);
-            playerKillPromise = createListenerPromise(stream, 'playerKill', onPlayerKill, 4);
-            let data: string = '';
-            for (let event of events) {
-                data += `${event.timestamp},${event.message}\n`;
-            }
+        before(async () => {
+            receivedEvents = {
+                playernames: [],
+                playerKill: []
+            };
 
-            // Act
+            stream.on('playernames', (arg) => {
+                receivedEvents.playernames.push(arg);
+            });
+            stream.on('playerKill', (arg) => {
+                receivedEvents.playerKill.push(arg);
+            });
+
+            let data: string = '';
+            for (let message of messages) {
+                data += `${message.timestamp},${message.data}\n`;
+            }
             stream.read(data);
+
+            await sleep(100);
         });
 
         it('should process playernames events', async () => {
-            await playerNamesPromise;
-            expect(playerNamesEvents.length).to.equal(1);
-            for (let expectedEvent of expectedPlayerNamesEvents) {
-                expect(playerNamesEvents).to.deep.include(expectedEvent);
+            expect(receivedEvents.playernames.length).to.equal(1);
+            for (let expectedEvent of expectedEvents.playernames) {
+                expect(receivedEvents.playernames).to.deep.include(expectedEvent);
             }
         });
 
         it('should process playerKill events', async () => {
-            await playerKillPromise;
-            expect(playerKillEvents.length).to.equal(4);
-            for (let expectedEvent of expectedPlayerKillEvents) {
-                expect(playerKillEvents).to.deep.include(expectedEvent);
+            expect(receivedEvents.playerKill.length).to.equal(4);
+            for (let expectedEvent of expectedEvents.playerKill) {
+                expect(receivedEvents.playerKill).to.deep.include(expectedEvent);
             }
-        });
-
-        after(() => {
-            removeAllListeners(stream);
         });
     });
 });
