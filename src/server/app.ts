ece84439d351a6799e1395ba22bbf732efa59d36
Kevin Snyder <kevipapo1@gmail.com>
2018-07-04 15:16:14 -0700

    Autoconnect to cab
    Running `npm run start` with no arguments will now look for IP addresses on the local network with port 12749 open. The first IP it finds with this port open is the one it will connect to.
    
    This still doesn't handle disconnects from the cab, so it is not fully persistent.

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,63 @@
 import * as fs from 'fs';
+import { Netmask } from 'netmask';
+import * as os from 'os';
+import * as portscanner from 'portscanner';
 import * as socket_io from 'socket.io';
 import { GameStats, KQStat } from '../lib/GameStats';
 import { KQCab } from '../lib/KQCab';
 import { KQStream, KQStreamOptions } from '../lib/KQStream';
 
+function getLocalIpAddresses(): string[] {
+    const res: string[] = [];
+    const ifaces = os.networkInterfaces();
+    for (let name of Object.keys(ifaces)) {
+        const iface = ifaces[name];
+        for (let info of iface)  {
+            if (info.family === 'IPv4' && !info.internal) {
+                const block = new Netmask(`${info.address}/24`);
+                block.forEach((ip) => {
+                    res.push(ip);
+                });
+            }
+        }
+    }
+    return res;
+}
+
+async function getCandidateIps(ips: string[], port: number): Promise<string[]> {
+    const res: string[] = [];
+    const promises = ips.map(async (ip) => {
+        try {
+            const status = await portscanner.checkPortStatus(port, ip, {
+                timeout: 5000
+            });
+            if (status === 'open') {
+                res.push(ip);
+            }
+        } catch {
+            return;
+        }
+    });
+    await Promise.all(promises);
+    return res;
+}
+
 async function main() {
-    if (process.argv.length !== 4 || ['-c', '-r'].indexOf(process.argv[2]) === -1) {
+    if (
+        !(process.argv.length === 4 || process.argv.length === 2) ||
+        (process.argv.length === 4 && ['-c', '-r'].indexOf(process.argv[2]) === -1)
+    ) {
         throw new Error('Incorrect usage!');
+    }
+
+    if (process.argv.length === 2) {
+        process.argv[2] = '-c';
+        const localIps = getLocalIpAddresses();
+        const candidateIps = await getCandidateIps(localIps, 12749);
+        if (candidateIps.length === 0) {
+            throw new Error('Could not find a Killer Queen cabinet on the local network');
+        }
+        process.argv[3] = `ws://${candidateIps[0]}:12749`;
     }
     
     const options: KQStreamOptions = {};
