27f6b3ad16f0b4c44566bce2f7046cfa2e84a309
Felix Yuan <jihadfeeves@gmail.com>
2019-09-22 13:40:52 -0700

    Add more stuffs

--- internal/app/client/parser.go
+++ internal/app/client/parser.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"regexp"
 	"strings"
+	"time"
+
+	"github.com/johncgriffin/overflow"
 )
 
 // StatParser is a struct that will turn stat messages into JSON blobs
@@ -21,20 +24,53 @@ func (p *StatParser) Parse(message string) (*Stat, error) {
 		return nil, errors.New("Not a stat")
 	}
 
-	// If we run into an error here, it wasn't valid anyway
-	splitMessage := strings.SplitN(message, ",", 2)
-	messageType := splitMessage[0]
-	keysAndValue := splitMessage[1]
+	// Check if timestamp is first
+	tsRegex := regexp.MustCompile(`[0-9]+,`)
+	idxs := tsRegex.FindStringIndex(message)
+
+	var tsString string
+	var ts time.Time
+	var keysAndValue string
+
+	if idxs != nil { // Found TS first
+		splitMessage := strings.SplitN(message, ",", 2)
+		tsString = splitMessage[0]
+		ts, err = p.convertTimestampString(tsString)
+		keysAndValue = splitMessage[1]
+	} else {
+		ts = time.Now()
+		keysAndValue = message
+	}
 
 	stat := Stat{
 		RawMessage: message,
-		StatType:   messageType,
+		Timestamp:  ts,
 	}
 	return &stat, nil
 }
 
 func (p *StatParser) validate(message string) bool {
 	// Valid messages match something that looks like TS, ![k[messageType], v[message,values]]!
-	messageRegex := regexp.MustCompile(`[0-9]{10,13},!\[k\[[a-zA-Z]+\],v\[[^\]]+\]\]!`)
+	messageRegex := regexp.MustCompile(`([0-9]+,)?!\[k\[[a-zA-Z]+\],v\[[^\]]+\]\]!`)
 	return messageRegex.MatchString(message)
+}
+
+func (p *StatParser) convertTimestampString(tsString string) time.Time {
+	i, err := strConv.ParseInt(tsString, 10, 64)
+	if err != nil {
+		return time.Now()
+	}
+
+	epoch := time.Date(1970, time.January, 1, 0, 0, 0, 0, time.UTC)
+	_, ok := overflow.Mul64(i, int64(time.Second))
+	var ts time.Time
+	if !ok {
+		ts = epoch.Add(i * time.Millisecond)
+	} else {
+		ts = epoch.Add(i * time.Second)
+	}
+	if ts.After(time.Now()) {
+		ts = time.Now()
+	}
+	return ts
 }
