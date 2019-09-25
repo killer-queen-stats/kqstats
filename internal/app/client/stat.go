0d94f0fe07f042014b31b2b9f2490f95e2c2ae00
Felix Yuan <jihadfeeves@gmail.com>
2019-09-24 21:20:25 -0700

    Add all stat types

--- internal/app/client/stat.go
+++ internal/app/client/stat.go
@@ -7,7 +7,25 @@ type StatType string
 
 // Declare all stat types below
 const (
-	Alive StatType = "alive"
+	Alive           StatType = "alive"
+	PlayerNames     StatType = "playernames"
+	PlayerKill      StatType = "playerKill"
+	BlessMaiden     StatType = "blessMaiden"
+	ReserveMaiden   StatType = "reserveMaiden"
+	UnreserveMaiden StatType = "unreserveMaiden"
+	UseMaiden       StatType = "useMaiden"
+	Glance          StatType = "glance"
+	CarryFood       StatType = "carryFood"
+	GameStart       StatType = "gamestart"
+	GameEnd         StatType = "gameend"
+	Victory         StatType = "victory"
+	Spawn           StatType = "spawn"
+	GetOnSnail      StatType = "getOnSnail"
+	GetOffSnail     StatType = "getOffSnail"
+	SnailEat        StatType = "snailEat"
+	SnailEscape     StatType = "snailEscape"
+	BerryDeposit    StatType = "berryDeposit"
+	BerryKickIn     StatType = "berryKickIn"
 )
 
 // Stat is a construct that envelopes everything that's spat out by the parser
