0254b217c11f318352a5742cbf0bcc02a5cb2ad3
Felix Yuan <jihadfeeves@gmail.com>
2019-09-24 21:46:53 -0700

    Add parsed values

--- internal/app/client/stat.go
+++ internal/app/client/stat.go
@@ -35,3 +35,61 @@ type Stat struct {
 	Timestamp  time.Time              `json:"timestamp"`
 	Payload    map[string]interface{} `json:"payload"`
 }
+
+func CreateAliveValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreatePlayerNamesValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreatePlayerKillValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateBlessMaidenValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateReserveMaidenValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateUnreserveMaidenValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateUseMaidenValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateGlanceValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateCarryFoodValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateGameStartValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateGameEndValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateVictoryValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateSpawnValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateGetOnSnailValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateGetOffSnailValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateSnailEatValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateSnailEscapeValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateBerryDepositValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
+func CreateBerryKickInValues(values string) (map[string]interface{}, error) {
+	return nil, nil
+}
